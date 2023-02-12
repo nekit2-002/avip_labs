4eeb3eb00595a9843cc828ea251a4b94715f2835
Nikita Kosenko <foxidalim@gmail.com>
2023-02-12 12:51:09 +0300

    Add some files for first laba.

---
+++ laba1/KNN_resampling.py
@@ -0,0 +1,8 @@
+from PIL import Image
+import numpy as np
+
+def KNN_resampling(old_image, scale):
+    pass
+
+if __name__ == '__main__':
+    pass
---
+++ laba1/bilinear_resampling.py
@@ -0,0 +1,5 @@
+from PIL import Image
+import numpy as np
+
+if __name__ == '__main__':
+    pass
