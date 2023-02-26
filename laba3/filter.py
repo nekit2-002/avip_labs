a662daa91d52567bf1bef9796da57631821afb97
Nikita Kosenko <foxidalim@gmail.com>
2023-02-26 17:59:52 +0300

    Changes in filter.py

--- laba3/filter.py
+++ laba3/filter.py
@@ -2,18 +2,17 @@ import numpy as np
 
 
 def apply_aperture(img, new_image, x, y, size, threshold):
-    low = max(y - size // 2, 0)
-    above = min(y + size // 2 + 1, img.shape[0])
-    left = max(x - size // 2, 0)
-    right = min(x + size // 2 + 1, img.shape[1])
+    left = max(y - size // 2, 0)
+    right = min(y + size // 2 + 1, img.shape[1])
+    low = max(x - size // 2, 0)
+    above = min(x + size // 2 + 1, img.shape[0])
     
-    aperture = img[low : above,
-                   left : right]
+    aperture = img[low : above, left : right]
     
     ones = (aperture == 255).sum()
 
     if ones >= threshold:
-        new_image[y, x] = 255
+        new_image[x, y] = 255
 
 
 def rank_filter(img, size, threshold):
@@ -24,7 +23,7 @@ def rank_filter(img, size, threshold):
 
     for x in range(img.shape[0]):
         for y in range(img.shape[1]):
-            apply_aperture(img, new_img, y, x, size, threshold)
+            apply_aperture(img, new_img, x, y, size, threshold)
             
     return new_img.astype(np.uint8)
 
