2ef23b848b2c7123b048ba3bc49ecf933abdfc26
Nikita Kosenko <foxidalim@gmail.com>
2023-04-01 13:18:38 +0300

    Add latest changes.

--- laba6/generate_sentence.py
+++ laba6/generate_sentence.py
@@ -2,11 +2,17 @@ from math import ceil
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from gen import FontDrawer
+from helpers import calculate_profile, cut_white
 
 SENTENCE = "𐒁𐒋𐒔𐒄𐒆𐒊 𐒇𐒘𐒝𐒏𐒆𐒎𐒓 𐒂𐒈𐒒𐒋𐒔𐒜𐒌 𐒍𐒗 𐒖𐒕𐒑𐒛𐒚"
 
 if __name__ == '__main__':
     util = FontDrawer()
-    result = util.render_binarized(SENTENCE, 200)
+    result = util.render_binarized(SENTENCE, 170)
+
+    for axis in (0, 1):
+        text_profile = calculate_profile(result, axis)
+        result, _ = cut_white(result, text_profile, axis)
+
 
     Image.fromarray(result,'L').save(f"results/sentence.bmp")
