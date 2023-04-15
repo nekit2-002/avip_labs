7e4bf8ae973a56b0a2cb60d3ef23572d3c153ccc
Nikita Kosenko <foxidalim@gmail.com>
2023-04-15 22:21:21 +0300

    Latest changes.

--- laba7/generate.py
+++ laba7/generate.py
@@ -1,8 +1,9 @@
 from os import path
 from math import ceil
+from helpers import np, cut_white, calculate_profile
 from PIL import Image, ImageFont, ImageDraw
 from fontTools.ttLib import TTFont
-from binarization import simple_bin, np
+from binarization import simple_bin
 
 
 font_path = path.join('fonts', 'NotoSansOsmanya-Regular.ttf')
@@ -16,7 +17,8 @@ osmanya = [
 
 
 def filename(n):
-    return f"alphabet/direct/letter_{str(n + 1).zfill(2)}.png"
+    # return f"alphabet/direct/letter_{str(n + 1).zfill(2)}.png"
+    return f"../laba6/results/symbols/letter_{n}.png"
 
 
 class FontDrawer:
@@ -28,9 +30,9 @@ class FontDrawer:
         self.s = self.font.getGlyphSet()
         self.units_per_em = self.font['head'].unitsPerEm
 
-    def render_text(self, text, total = 170):
+    def render_text(self, text):
         img = Image.new(mode="RGB",
-                        size=(ceil(self.get_text_width(text, font_size, total)),
+                        size=(ceil(self.get_text_width(text, font_size)),
                               font_size),
                         color="white")
 
@@ -39,8 +41,8 @@ class FontDrawer:
 
         return img
 
-    def render_binarized(self, text, total = 170, level = 100):
-        img = self.render_text(text, total)
+    def render_binarized(self, text, level=100):
+        img = self.render_text(text)
         return 255 - simple_bin(np.array(img), level)
 
     def get_char_width(self, c, point_size):
@@ -54,7 +56,9 @@ class FontDrawer:
 
         return pts * float(point_size) / self.units_per_em
 
-    def get_text_width(self, text, point_size, total = 170):
+    def get_text_width(self, text, point_size):
+        total = 100
+
         for c in text:
             total += self.get_char_width(c, point_size)
 
@@ -65,3 +69,16 @@ def save_arr_as_img(arr, file_name):
     binarized_letter = Image.fromarray(255 - arr, 'L')
     binarized_letter = binarized_letter.convert('1')
     binarized_letter.save(file_name)
+
+
+if __name__ == '__main__':
+    font_drawer = FontDrawer()
+    # letters that can merge
+    for i, letter in enumerate(osmanya):
+        binarized_arr = font_drawer.render_binarized(letter)
+        # Delete white around letter
+        for axis in (0, 1):
+            letter_profile = calculate_profile(binarized_arr, axis)
+            binarized_arr, _ = cut_white(binarized_arr, letter_profile, axis)
+
+        save_arr_as_img(binarized_arr, filename(i))
