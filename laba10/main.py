9519d5aa179c72fd3226b991a6e7d22f1415e027
Nikita Kosenko <foxidalim@gmail.com>
2023-05-20 20:57:13 +0300

    Finish the last laba.

--- laba10/main.py
+++ laba10/main.py
@@ -22,26 +22,36 @@ if __name__ == '__main__':
 
    dpi = 500
    spectogram_a, frequencies_a = spectrogram_plot(samples_a, sample_rate_a, 11000)
-#    plt.axhline(y = 602,  color = 'r', linestyle = '-', lw= 0.5)
+   plt.axhline(y = 602,  color = 'r', linestyle = '-', lw= 0.5, label = "Форманты")
+   plt.axhline(y = 861,  color = 'r', linestyle = '-', lw= 0.5)
+   plt.axhline(y = 1119,  color = 'r', linestyle = '-', lw= 0.5)
+   plt.legend()
    plt.savefig('results/spectrogram_a.png', dpi = dpi)
    plt.clf()
 
    changeSampleRate("voice_i.wav")
    sample_rate_i , samples_i = wavfile.read("results/wavs/voice_i.wav")
    spectogram_i, frequencies_i = spectrogram_plot(samples_i, sample_rate_i, 11000)
-#    plt.axhline(y = 86,  color = 'r', linestyle = '-', lw= 0.5)
+   plt.axhline(y = 344,  color = 'r', linestyle = '-', lw= 0.5, label = "Форманты")
+   plt.axhline(y = 602,  color = 'r', linestyle = '-', lw= 0.5)
+   plt.axhline(y = 1894,  color = 'r', linestyle = '-', lw= 0.5)
+   plt.legend()
    plt.savefig('results/spectrogram_i.png', dpi = dpi)
    plt.clf()
 
    changeSampleRate("voice_gav.wav")
    sample_rate_gav , samples_gav = wavfile.read("results/wavs/voice_gav.wav")
    spectogram_gav, frequencies_gav = spectrogram_plot(samples_gav, sample_rate_gav, 11000)
+   plt.axhline(y = 344,  color = 'r', linestyle = '-', lw= 0.5, label = "Форманты")
+   plt.axhline(y = 602,  color = 'r', linestyle = '-', lw= 0.5)
+   plt.axhline(y = 1119,  color = 'r', linestyle = '-', lw= 0.5)
+   plt.legend()
    plt.savefig('results/spectrogram_gav.png', dpi = dpi)
    plt.clf()
 
    spec_a = integral_image(spectogram_a)
    
-   formants_a = list(find_all_formants(frequencies_a, spec_a, 1))
+   formants_a = list(find_all_formants(frequencies_a, spec_a, 3))
    formants_a.sort()
 
    print("Минимальная частота для звука А: " + str(formants_a[0]))
@@ -49,7 +59,7 @@ if __name__ == '__main__':
 
    print("Тембрально окрашенный тон для звука А: " + str(formants_a[0]))
 
-   power_a = power(frequencies_a, spec_a, 1, formants_a)
+   power_a = power(frequencies_a, spec_a, 3, formants_a)
    print(sorted(power_a.values()))
    print(sorted(power_a.items(), key = lambda item: item[1], reverse=True))
    print("Три самые сильные форманты: " + str(sorted(power_a, key=lambda i: power_a[i])[-3:]))
@@ -74,10 +84,10 @@ if __name__ == '__main__':
    formants_gav = list(find_all_formants(frequencies_gav, spec_gav, 1))
    formants_gav.sort()
 
-   print("Минимальная частота для звука А: " + str(formants_gav[0]))
-   print("Максимальная частота для звука А: " + str(formants_gav[-1]))
+   print("Минимальная частота для звука ГАВ: " + str(formants_gav[0]))
+   print("Максимальная частота для звука ГАВ: " + str(formants_gav[-1]))
 
-   print("Тембрально окрашенный тон для звука А: " + str(formants_gav[0]))
+   print("Тембрально окрашенный тон для звука ГАВ: " + str(formants_gav[0]))
 
    power_gav = power(frequencies_gav, spec_gav, 1, formants_gav)
    print(sorted(power_a.values()))
